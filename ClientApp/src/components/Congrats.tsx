753df123a922bf34a3945643c7b358aeaa51c724
maciej <maciej0905@gmail.com>
2023-04-21 23:31:54 +0200

    add more confetti

--- ClientApp/src/components/Congrats.tsx
+++ ClientApp/src/components/Congrats.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import ConfettiExplosion from "react-confetti-explosion";
+import ReactConfetti from "react-confetti";
 
 interface CongratsProps {
   onShowTask: () => void;
@@ -13,7 +14,12 @@ const Congrats: React.FC<CongratsProps> = ({ onShowTask }) => {
       <Grid item>
         <Typography variant="h4">Gratulacje! Miejsce znalezione!</Typography>{" "}
       </Grid>
-      <ConfettiExplosion />
+      {Number(localStorage.getItem("taskNumber")) % 2 === 0 ? (
+        <ConfettiExplosion />
+      ) : (
+        <ReactConfetti />
+      )}
+
       <Grid item>
         <Typography variant="h6">
           Kliknij na poniższy przycisk aby wyświetlić zadanie:
